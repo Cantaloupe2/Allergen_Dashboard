a9023358b396d127bf632a1d46c506f1d926b700
Benjamin Mellon <97412534+Cantaloupe2@users.noreply.github.com>
2023-10-25 20:24:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,52 @@ plt.show()
 st.pyplot()
 st.write(yearsdf.value_counts().sort_index())
 #st.bar_chart(yearsdf.value_counts())
+
+
+
+
+#########
+
+import pandas as pd
+
+df = pd.read_csv('Diet Data No Names.csv')
+sdf=df.drop(['Major','RCPD','Other','Notes','Dining Accommodation',
+             'Specialist','Intial Concern','Hall (Living/Eating)',
+             'Class Type','Date \nContacted'], axis=1)
+
+cdf=sdf.dropna(how='all')
+cdf
+
+ndf = cdf.fillna(0)
+
+columns_to_convert = ['Eggs', 'Milk', 'Fish','Shellfish','Peanuts',
+                      'Tree Nuts','Sesame','Soy','Wheat/Gluten','Vegan',
+                      'Vegetarian','Halal','Kosher']
+
+for col in columns_to_convert:
+    ndf[col] = ndf[col].replace('x', 1)
+    
+matrix = ndf.corr()
+print(matrix)
+
+from string import ascii_letters
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set_theme(style="white")
+
+# Generate a mask for the upper triangle
+mask = np.triu(np.ones_like(matrix, dtype=bool))
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(11, 11))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(matrix, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+st.pyplot()
