2412295a5187e87db17e955170046ff34496d38c
Benjamin Mellon <97412534+Cantaloupe2@users.noreply.github.com>
2023-12-06 20:07:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,8 +84,8 @@ if add_sidebar == "1":
 
   for col in columns_to_convert:
       ndf[col] = ndf[col].replace('x', 1)
+      st.write(ndf[col].unique())
   st.write(ndf)
-  st.write(ndf.unique())
   matrix = ndf.corr()
   print(matrix)
 
