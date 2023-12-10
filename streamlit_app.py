cc87b167d299be44578fcdd81a0456340b267382
Benjamin Mellon <97412534+Cantaloupe2@users.noreply.github.com>
2023-12-09 19:28:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,7 +152,7 @@ st.write(stacked_df)
 st.write(by_year)
 total_concerns = []
 for i in range(len(stacked_df)):
-  total_concerns.append(stacked_df.iloc[i,"Prop"]*by_year[stacked_df.iloc[i,"year"]])
+  total_concerns.append(stacked_df.iloc[i,"Prop"]*by_year[stacked_df.iloc[i,"Year"]])
 
 # non proportional dietary condition plot
 fig4 = px.line(x = by_year.index, y = total_concerns, color = stacked_df["allergy"])
