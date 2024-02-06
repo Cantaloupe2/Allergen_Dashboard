4b07577e438e15b246cf2153969ef677ab879cc1
Benjamin Mellon <97412534+Cantaloupe2@users.noreply.github.com>
2024-02-05 23:35:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,14 +33,14 @@ def format_date(date_str):
     try:
         # Attempt to parse the date
         parsed_date = datetime.strptime(date_str, '%m/%d/%y')
-        st.write(parsed_date.strftime('%m/%d/%yy'))
-        return parsed_date.strftime('%m/%d/%yy')
+        st.write(parsed_date.strftime('%m/%d/%y'))
+        return parsed_date.strftime('%m/%d/%y')
     except ValueError:
         # If parsing fails, try another format
         try:
             parsed_date = datetime.strptime(date_str, '%m/%d/%Y %H:%M:%S')
-            parsed_date.strftime('%m/%d/%yy')
-            return parsed_date.strftime('%m/%d/%yy')
+            parsed_date.strftime('%m/%d/%y')
+            return parsed_date.strftime('%m/%d/%y')
         except ValueError:
             # Handle the case where the date is not in the expected formats
             st.write(date_str)
