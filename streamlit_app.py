e19b42ab34d52fd49354015366cef9950022f8fc
Benjamin Mellon <97412534+Cantaloupe2@users.noreply.github.com>
2024-02-05 23:40:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -159,7 +159,7 @@ fig2.update_layout(
 
 dates_ndf = ndf
 dates_ndf["dates"] = df["dates"]
-# st.write(df["dates"])
+st.write(df["dates"])
 # create a list of the "Date \nContacted" column in diet_data and name it dates
 dates_series = dates_ndf['dates']
 # st.write(dates_series)
